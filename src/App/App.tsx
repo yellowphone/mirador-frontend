36892fc42cfc7d7ded44b5c3315b8f7e631051a2
baileyg2016 <bspell20@vt.edu>
2020-11-22 12:34:15 -0500

    removing some stuff

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,12 @@
-import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { HelloWorld } from '../components/HelloWorld';
 
 export const App: React.FC = () => {
 
     return (
         <>
-            <h1>lets get this going</h1>	  
+            <h1>Hey Geo</h1>	  
         </>
     );
 }
 
-// export default hot(App);
 export default App;
