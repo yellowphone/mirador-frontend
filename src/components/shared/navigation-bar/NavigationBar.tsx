5af068f8bc72c563924cf09ebc83aca9b8a65b63
baileyg2016 <bspell20@vt.edu>
2020-11-24 22:25:46 -0500

    home page basics and navbar are done

--- src/components/shared/navigation-bar/NavigationBar.tsx
+++ src/components/shared/navigation-bar/NavigationBar.tsx
@@ -4,6 +4,7 @@ import { Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import { useHistory } from 'react-router-dom';
 import { Paths } from '../../../utils/paths';
+import './NavigationBar.css'
 
 export const NavigationBar = () => {
     const history = useHistory();
@@ -15,15 +16,22 @@ export const NavigationBar = () => {
     return (
         <Navbar bg="light justify-content-between" sticky='top'>
             <Flex>
-                <Box p="4" >
+                <Box p='4'>
                     <Navbar.Brand onClick={() => onNavigate(Paths.Home)}>Mirador</Navbar.Brand>
                 </Box>
                 <Spacer />
-                <Box p="4" >
-                    <Nav className='mr-auto'>
+                <Box size='50%' pr='85%' pl='85%' pt='4' >
+                    <Nav className='center'>
                         <Nav.Link onClick={() => onNavigate(Paths.Adventure)}>Adventures</Nav.Link>
                         <Nav.Link onClick={() => onNavigate(Paths.Blog)}>Blogs</Nav.Link>
                         <Nav.Link onClick={() => onNavigate(Paths.Itenerary)}>Iteneraries</Nav.Link>
+                    </Nav>
+                </Box>
+                <Spacer />
+                <Box p='4'>
+                    <Nav>
+                        <Nav.Link>Login</Nav.Link>
+                        <Nav.Link>Sign up</Nav.Link>
                     </Nav>
                 </Box>
             </Flex>
