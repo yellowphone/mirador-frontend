759da0ea31d39f80074f85c6efcf44603ca38a9b
geomin76 <geom@vt.edu>
2020-12-12 19:52:49 -0500

    Adding Search and reorganizing

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,7 +3,7 @@ import { NavigationBar } from '../shared/navigation-bar/NavigationBar';
 import { Input, VStack, Center, Box, } from '@chakra-ui/react';
 import './Home.css';
 
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { Search } from '../shared/Google/Search'
 
 export const Home = () => {
 
@@ -14,14 +14,7 @@ export const Home = () => {
                 <Box width='50%'>
                     <Center>Let's Explore</Center>
                     {/* <Input placeholder={'Type a location to get started'} /> */}
-
-                    {/* For now, this package can chill, but we can look for better options soon */}
-                    <GooglePlacesAutocomplete
-                        apiKey={process.env.MAPS_API_KEY}
-                        selectProps= {{
-                            placeholder: 'Type a location to get started',
-                        }}
-                    />
+                    <Search/>
                 </Box>
             </VStack>
         </>
