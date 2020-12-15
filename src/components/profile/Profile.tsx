aa02bacfd0c8806665ec502240a26139807c4446
baileyg2016 <bspell20@vt.edu>
2020-12-14 20:42:50 -0500

    working on profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavigationBar } from '../shared/navigation-bar/NavigationBar';
 import { ActionBar } from './ActionBar';
+import { ProfileCard } from './ProfileCard';
 
 export const Profile = () => {
 
     return (
         <>
             <NavigationBar />
+            <ProfileCard />
             <ActionBar />
-            <h1>Profile</h1>
         </>
     );
 }
