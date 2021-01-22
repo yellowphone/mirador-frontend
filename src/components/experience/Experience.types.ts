51aa1b60e2e2274b55b11cd331f1f5e9272e77c9
geomin76 <geom@vt.edu>
2021-01-22 14:06:41 -0500

    Fixing experience pages

--- src/components/experience/Experience.types.ts
+++ src/components/experience/Experience.types.ts
@@ -1,17 +1,27 @@
 import { DifficultyType } from '../shared/media/Badges/Badges.types';
 
 export interface IExperience {
+    fk_experience_location: number,
     imageUrl: string
     imageAlt: string
-    length: number
+    miles: number
     elevation: number
     title: string
+    summary: string
     rating: number
     lat: number
     lng: number
     difficulty: DifficultyType
 }
 
+export interface ICoordinates {
+    lat: number
+    lng: number
+}
+
 export interface ExperienceDataProps {
     experiences: Array<IExperience>;
+    coords: ICoordinates;
+    setCoords: Function;
+    refetch: any;
 }
