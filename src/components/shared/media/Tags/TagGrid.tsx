29c136f6196aabe28b3f38ab015d0709962a9951
geomin76 <geom@vt.edu>
2021-01-27 14:57:39 -0500

    Adding tag init files

---
+++ src/components/shared/media/Tags/TagGrid.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { TagGridDataProps } from './Tag.types';
+import { Tag } from './Tag';
+
+export const TagGrid: FC<TagGridDataProps> = ({ tags }) => {
+
+    // using a list of tags, render many one and/or more tags
+
+    return (
+        <>
+        </>
+    )
+}
