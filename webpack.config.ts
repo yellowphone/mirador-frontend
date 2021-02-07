d8ac9c707962fce805af60c695f28fe3c7cc0bd1
baileyg2016 <bspell20@vt.edu>
2021-02-07 15:19:05 -0500

    updating the html-webpack-plugin package

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,6 @@
 import { resolve } from 'path';
 import webpack from 'webpack';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 const dotenv = require('dotenv');
 
 module.exports = () => {
