ddaad5079dbe2cc3205ad6d0ae9834197ea0acea
geomin76 <geom@vt.edu>
2021-02-22 00:07:45 -0500

    Adding arrows

--- src/components/blog/single-blog/SingleBlog.tsx
+++ src/components/blog/single-blog/SingleBlog.tsx
@@ -1,10 +1,15 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Container, SimpleGrid, Center, Heading, VStack, Text, Image, Box } from "@chakra-ui/react"
 import { NavigationBar } from '../../shared/navigation-bar/NavigationBar';
 import { SingleBlogDataProps } from './SingleBlog.types'
 import { TagGrid } from '../../shared/media/Tags/TagGrid';
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons"
+
 
 export const SingleBlog: FC<SingleBlogDataProps> = ({ data, html }) => {
+
+    const [ leftArrowHover, setLeftArrowHover ] = useState(false);
+    const [ rightArrowHover, setRightArrowHover ] = useState(false);
 
     return(
         <>
@@ -25,9 +30,28 @@ export const SingleBlog: FC<SingleBlogDataProps> = ({ data, html }) => {
                     <Text>lat: {data["findBlogById"]["blog_locations"]["lat"]}, lng: {data["findBlogById"]["blog_locations"]["lng"]}</Text>
                 </Center>
                 { html }
-
             </VStack>
-        </Container>         
+        </Container> 
+        <Container maxW="sm" centerContent>
+            <SimpleGrid columns={2} spacing={10}>
+                <Box 
+                    onMouseEnter={() => { setLeftArrowHover(true) }}
+                    onMouseLeave={() => { setLeftArrowHover(false) }}
+                    d="flex" alignItems="center"
+                >
+                    <ChevronLeftIcon/>
+                    { leftArrowHover && <Text>Previous blog</Text> }
+                </Box>
+                <Box 
+                    onMouseEnter={() => { setRightArrowHover(true) }}
+                    onMouseLeave={() => { setRightArrowHover(false) }}
+                    d="flex" alignItems="center"
+                >
+                    { rightArrowHover && <Text >Next blog</Text>}
+                    <ChevronRightIcon />
+                </Box>
+            </SimpleGrid>
+        </Container>        
         </>
     )
 }
