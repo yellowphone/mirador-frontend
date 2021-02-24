5d2a8cd24f649c226a1007466a39e2a7912179a2
geomin76 <geom@vt.edu>
2021-02-24 15:26:02 -0500

    Adding cookies package and setup

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationBar } from '../shared/navigation-bar/NavigationBar';
 import { Input, VStack, Center, Box, } from '@chakra-ui/react';
+import { useCookies } from 'react-cookie';
 import './Home.css';
 
 import { Search } from '../shared/Google/Search'
 
 export const Home = () => {
+
+    const [cookies, setCookie] = useCookies(['name']);
+
+    useEffect(() => {
+        setCookie('name', "woo", { path: '/' });
+    })
 
     return (
         <>
@@ -15,7 +22,7 @@ export const Home = () => {
                     <Center>Let's Explore</Center>
                     <Input placeholder={'Type a location to get started'} />
                     {/* <SearchWithMap /> */}
-                    
+                    {cookies.name && <h1>Hello {cookies.name}</h1>}
                 </Box>
             </VStack>
         </>
