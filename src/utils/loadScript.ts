8c625df5816889a50a1646521b6cefb563f35fcc
Kaity Hallman <kaityh@spotify.com>
2021-04-01 18:24:23 -0400

    lint/fix utils/

--- src/utils/loadScript.ts
+++ src/utils/loadScript.ts
@@ -1,16 +1,22 @@
-
-export default (d: Document, s: string, id: string, jsSrc: string, cb: (resolve: any) => Promise<any>, onError: (error: any) => void) => {
-  const element = d.getElementsByTagName(s)[0]
-  const fjs = element
-  let js = element
-  js = d.createElement(s)
-  js.id = id
-  js.src = jsSrc
+export default (
+  d: Document,
+  s: string,
+  id: string,
+  jsSrc: string,
+  cb: ((this: GlobalEventHandlers, ev: Event) => never) | null,
+  onError: OnErrorEventHandler
+): void => {
+  const element = d.getElementsByTagName(s)[0] as HTMLScriptElement;
+  const fjs = element;
+  let js = element;
+  js = d.createElement(s) as HTMLScriptElement;
+  js.id = id;
+  js.src = jsSrc;
   if (fjs && fjs.parentNode) {
-    fjs.parentNode.insertBefore(js, fjs)
+    fjs.parentNode.insertBefore(js, fjs);
   } else {
-    d.head.appendChild(js)
+    d.head.appendChild(js);
   }
-  js.onerror = onError
-  js.onload = cb
-}
+  js.onerror = onError;
+  js.onload = cb;
+};
