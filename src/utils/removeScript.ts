3089ad07e079c0c769c93d5348e30a25860c3e01
Kaity Hallman <kaityhallman@gmail.com>
2021-04-05 18:10:50 -0400

    lint/fix utils/

--- src/utils/removeScript.ts
+++ src/utils/removeScript.ts
@@ -1,7 +1,7 @@
-export default (d: Document, id: string) => {
-    const element = d.getElementById(id)
-  
-    if (element && element.parentNode) {
-      element.parentNode.removeChild(element)
-    }
+export default (d: Document, id: string): void => {
+  const element = d.getElementById(id);
+
+  if (element && element.parentNode) {
+    element.parentNode.removeChild(element);
   }
+};
