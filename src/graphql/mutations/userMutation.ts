225f96d3cf4c3d29c0993c012faf18e67bd103d3
Kaity Hallman <kaityhallman@gmail.com>
2021-04-05 18:10:49 -0400

    lint/fix graphql/

--- src/graphql/mutations/userMutation.ts
+++ src/graphql/mutations/userMutation.ts
@@ -1,34 +1,48 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_USER = gql`
-    mutation createUser($email: String!, $firstname: String!, $lastname: String!, $access_token: String!, $user_id: String!, $image_url: String!) {
-        createUser(email: $email, firstname: $firstname, lastname: $lastname, access_token: $access_token, user_id: $user_id, image_url: $image_url) {
-            pkuser
-            email
-            firstname
-            lastname
-            username
-            image_url
-            access_token
-            account_type
-        }
+  mutation createUser(
+    $email: String!
+    $firstname: String!
+    $lastname: String!
+    $access_token: String!
+    $user_id: String!
+    $image_url: String!
+  ) {
+    createUser(
+      email: $email
+      firstname: $firstname
+      lastname: $lastname
+      access_token: $access_token
+      user_id: $user_id
+      image_url: $image_url
+    ) {
+      pkuser
+      email
+      firstname
+      lastname
+      username
+      image_url
+      access_token
+      account_type
     }
+  }
 `;
 
 export const FOLLOW_USER = gql`
-    mutation followUser($user_following: Int!, $user_followed: Int!) {
-        followUser(user_following: $user_following, user_followed: $user_followed)
-    }
+  mutation followUser($user_following: Int!, $user_followed: Int!) {
+    followUser(user_following: $user_following, user_followed: $user_followed)
+  }
 `;
 
 export const UNFOLLOW_USER = gql`
-    mutation unfollowUser($pkfollower: Int!) {
-        unfollowUser(pkfollower: $pkfollower)
-    }
+  mutation unfollowUser($pkfollower: Int!) {
+    unfollowUser(pkfollower: $pkfollower)
+  }
 `;
 
 export const DELETE_USER = gql`
-    mutation deleteUser($pkuser: Int!) {
-        deleteUser(pkuser: $pkuser)
-    }
+  mutation deleteUser($pkuser: Int!) {
+    deleteUser(pkuser: $pkuser)
+  }
 `;
--- src/graphql/queries/blogQuery.ts
+++ src/graphql/queries/blogQuery.ts
@@ -1,80 +1,80 @@
 import { gql } from '@apollo/client';
 
 export const FIND_BLOG_BY_ID = gql`
-    query findBlogById($pkblog: Int!) {
-        findBlogById(pkblog: $pkblog) {
-            pkblog
-            title
-            summary
-            content
-            created_on
-            comment_blogs {
-                comment
-                users {
-                    username
-                }
-            }
-            blog_locations {
-                lat
-                lng
-            }
-            blog_tags {
-                pkblog_tag
-                tags {
-                  tag
-                }
-            }
+  query findBlogById($pkblog: Int!) {
+    findBlogById(pkblog: $pkblog) {
+      pkblog
+      title
+      summary
+      content
+      created_on
+      comment_blogs {
+        comment
+        users {
+          username
         }
+      }
+      blog_locations {
+        lat
+        lng
+      }
+      blog_tags {
+        pkblog_tag
+        tags {
+          tag
+        }
+      }
     }
+  }
 `;
 
 export const FIND_MANY_BLOGS = gql`
-    query findManyBlogs {
-        findManyBlogs {
-            pkblog
-            title
-            summary
-            public_identifier
-            blog_tags {
-                pkblog_tag
-                tags {
-                  tag
-                }
-            }
+  query findManyBlogs {
+    findManyBlogs {
+      pkblog
+      title
+      summary
+      public_identifier
+      blog_tags {
+        pkblog_tag
+        tags {
+          tag
         }
+      }
     }
+  }
 `;
 
 export const FIND_RANDOM_BLOG = gql`
-    query {
-        findRandomBlog(previousPrimaryKey:1)
-    }
+  query {
+    findRandomBlog(previousPrimaryKey: 1)
+  }
 `;
 
 export const FIND_BLOG_BY_PUBLIC_IDENTIFIER = gql`
-    query findBlogByPublicIdentifier($public_identifier: String!) {
-        findBlogByPublicIdentifier(public_identifier: $public_identifier) {
-            pkblog
-            title
-            summary
-            content
-            created_on
-            comment_blogs {
-                comment
-                users {
-                    username
-                }
-            }
-            blog_locations {
-                lat
-                lng
-            }
-            blog_tags {
-                pkblog_tag
-                tags {
-                  tag
-                }
-            }
+  query findBlogByPublicIdentifier($public_identifier: String!) {
+    findBlogByPublicIdentifier(public_identifier: $public_identifier) {
+      pkblog
+      title
+      summary
+      content
+      created_on
+      comment_blogs {
+        comment
+        users {
+          username
         }
+      }
+      blog_locations {
+        lat
+        lng
+      }
+      blog_tags {
+        pkblog_tag
+        tags {
+          tag
+        }
+      }
     }
+  }
 `;
