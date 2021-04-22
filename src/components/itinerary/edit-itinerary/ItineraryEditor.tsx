7a5f46ca5c7d00d62774c54c6c41c0e1237086d8
geomin76 <geom@vt.edu>
2021-04-22 19:23:53 -0400

    Initial commit for edit

---
+++ src/components/itinerary/edit-itinerary/ItineraryEditor.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement, useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { mongodbClient } from '../../../graphql/mongodbClient';
+import { EmptyItinerary } from './EmptyItinerary';
+import { ActiveItinerary } from '../ActiveItinerary';
+import { ManyElementDataProps } from './EditItinerary.types';
+import { CREATE_MONGODB_ITINERARY } from '../../../graphql/mutations/mongodbMutation';
+
+export const ItineraryEditor = (): ReactElement => {
+  const [mongoid, setMongoid] = useState('');
+  const [elements, setElements] = useState<ManyElementDataProps>({});
+
+  const [createMongoItinerary] = useMutation(CREATE_MONGODB_ITINERARY, {
+    client: mongodbClient,
+  });
+
+  // Itinerary creates
+  const onItineraryCreate = (input: { start: string; end: string }) => {
+    if (input['start'] <= input['end']) {
+      createMongoItinerary({
+        variables: {
+          beginning: input['start'],
+          end: input['end'],
+        },
+      }).then(data => {
+        setMongoid(data.data.createItinerary._id);
+        delete data.data.createItinerary._id;
+        setElements(data.data.createItinerary);
+      });
+    } else {
+      alert('Date range is not valid! Try again!');
+    }
+  };
+
+  if (Object.keys(elements).length === 0) {
+    return <EmptyItinerary onItineraryCreate={onItineraryCreate} />;
+  } else {
+    return (
+      <ActiveItinerary
+        elements={elements}
+        setElements={setElements}
+        mongoId={mongoid}
+      />
+    );
+  }
+};
---
+++ src/components/itinerary/edit-itinerary/ItinerarySearcher.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+import { FIND_EXPERIENCE_BY_COORDINATES } from '../../../graphql/queries/experienceQuery';
+import { Search } from '../../shared/Google/Search';
+import { Map } from '../../shared/Google/Map';
+import { IExperience } from '../../experience/Experience.types';
+import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
+
+const ItinerarySearcherContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ItinerarySearcher = (): React.ReactElement => {
+  const [coords, setCoords] = useState({ lat: 37.235961, lng: -80.607775 });
+  const { lat, lng } = coords;
+
+  const loader = new Loader({
+    apiKey: `${process.env.MAPS_API_KEY}`,
+    version: 'weekly',
+    libraries: ['places', 'geometry'],
+  });
+
+  const { data: experienceItems, loading, error } = useQuery(
+    FIND_EXPERIENCE_BY_COORDINATES,
+    {
+      variables: { lat, lng },
+    }
+  );
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    console.error(error);
+    return <h1>Error!</h1>;
+  }
+
+  const experienceList: Array<IExperience> = experienceItems?.findExperienceByCoordinates?.map(
+    (item: IExperience) => {
+      return {
+        fk_experience_location: item.fk_experience_location,
+        imageUrl: 'http://www.citrusmilo.com/acadia/joebraun_precipice27.jpg',
+        imageAlt: 'ok',
+        miles: item.miles,
+        elevation: item.elevation,
+        title: item.title,
+        summary: item.summary,
+        rating: 4,
+        lat: item.lat,
+        lng: item.lng,
+        difficulty: item.difficulty,
+      };
+    }
+  );
+
+  return (
+    <ItinerarySearcherContainer>
+      <Search setCoords={setCoords} loader={loader} />
+      <Map
+        loader={loader}
+        coords={coords}
+        experiences={experienceList}
+        infoWindow={true}
+      />
+    </ItinerarySearcherContainer>
+  );
+};
