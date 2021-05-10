7c2050e27511ee344fb78bb8eb22ad126cede1b3
geomin76 <geom@vt.edu>
2021-05-10 12:05:09 -0400

    Added graphql mongo and init edit blog files

---
+++ src/components/blog/edit-blog/ConnectedEditBlog.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { EditBlog } from './EditBlog';
+
+export const ConnectedEditBlog = () => {
+  return <EditBlog />;
+};
---
+++ src/components/blog/edit-blog/EditBlog.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EditBlog = () => {
+  return <p>hello</p>;
+};
