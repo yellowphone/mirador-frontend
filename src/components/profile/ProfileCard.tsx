84b45cc5b1aad85da9209a035c16cc9cbf3ed829
Kaity Hallman <kaityhallman@gmail.com>
2021-05-24 16:01:44 -0400

    fun on profiile page

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,36 +1,81 @@
-import { Badge, Box, Image, Stack } from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
+import {
+  Badge,
+  Box,
+  Heading,
+  Text,
+  Flex,
+  Avatar,
+  HStack,
+} from '@chakra-ui/react';
 import React from 'react';
+import { gradientRanges } from '../../utils/styles/gradients';
 import { getUserContext } from '../../utils/userContext';
 
+const randomGradient =
+  gradientRanges[Math.floor(Math.random() * gradientRanges.length)];
+
+const Count = ({
+  number,
+  text,
+  last = false,
+}: {
+  number: number;
+  text: string;
+  last?: boolean;
+}) => {
+  return (
+    <Flex direction="column" marginRight={!last ? '16px' : undefined}>
+      <Text fontSize="3xl" fontWeight="bold">
+        {number}
+      </Text>
+      <Text>{text}</Text>
+    </Flex>
+  );
+};
 export const ProfileCard = (): React.ReactElement => {
   const user = getUserContext();
 
   return (
-    <Box
-      margin="auto"
-      maxW="30em"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-    >
-      <Stack direction="row">
-        <Image
-          src={user?.image_url}
-          alt="Profile picture"
-          borderRadius="full"
-          boxSize="150px"
-        />
-        <Box p="6">
-          <Box>{user?.email}</Box>
-          <Box>100 Followers &bull; 50 Following</Box>
-          <Box>{user?.fullname}</Box>
-          <Stack direction="row" wrap="wrap">
+    <>
+      <Box w="100%" h="175px" bgGradient={`linear(to-l, ${randomGradient})`} />
+      <Box margin="0 24px">
+        <Flex margin="16px 16px 40px 16px">
+          <Avatar
+            src={user?.image_url}
+            name={user?.fullname}
+            showBorder
+            borderWidth="5px"
+            borderRadius="100%"
+            size="2xl"
+            marginTop="-64px"
+            marginRight="16px"
+          />
+          <Flex direction="column">
+            <Heading as="h1" size="lg">
+              {user?.fullname}
+            </Heading>
+            <Flex alignItems="center">
+              <EmailIcon marginRight="2" />
+              <Text fontSize="sm">{user?.email}</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex margin="16px" direction="column">
+          <Heading as="h2" size="sm" marginBottom="8px">
+            Tags
+          </Heading>
+          <HStack spacing="8px">
             <Badge>Climbing</Badge>
             <Badge>Hiking</Badge>
             <Badge>Canoing</Badge>
-          </Stack>
-        </Box>
-      </Stack>
-    </Box>
+          </HStack>
+        </Flex>
+        <Flex margin="16px">
+          <Count number={100} text="Followers" />
+          <Count number={50} text="Following" />
+        </Flex>
+      </Box>
+    </>
   );
 };
---
+++ src/utils/styles/gradients.ts
@@ -0,0 +1,7 @@
+export const gradientRanges = [
+  '#3a1c71, #d76d77, #ffaf7b', // relay
+  '#ff5f6d, #ffc371', // sweet morning
+  '#02aab0, #00cdac', // green beach
+  '#48c6ef, #6f86d6', // fly high
+  '#B7F8DB, #50A7C2', // morning salad
+];
