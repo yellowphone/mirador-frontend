4985b36f89a0a0fd0e6120578092a27b654accb1
Kaity Hallman <kaityhallman@gmail.com>
2021-05-25 11:15:43 -0400

    update types

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { TSFixMe } from '../../types/global';
 import { NavigationBar } from '../shared/navigation-bar/NavigationBar';
 import { ActionBar } from './ActionBar';
 import { ProfileCard } from './ProfileCard';
 
-export const Profile = ({
-  userData,
-}: {
-  userData: TSFixMe;
-}): React.ReactElement => {
+export const Profile = (): React.ReactElement => {
   return (
     <>
       <NavigationBar />
       <ProfileCard />
-      <ActionBar userData={userData} />
+      <ActionBar />
     </>
   );
 };
