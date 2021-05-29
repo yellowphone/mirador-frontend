e2ff1fdaaf01f2f104725390e43586a2a63e6eda
geomin76 <geom@vt.edu>
2021-05-29 17:32:39 -0400

    removing comments

--- src/graphql/mongodbClient.ts
+++ src/graphql/mongodbClient.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const mongodbClient = new ApolloClient({
-  // uri: 'http://localhost:4001/graphql',
   uri: process.env.MONGODB_API_URL,
   cache: new InMemoryCache(),
 });
